b891feac955484908bd97f3a3e0208bce1fd2452
Yi-Chieh Weng <yichieh.weng@gmail.com>
2017-08-03 22:07:52 +0800

    Day1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("***********");
+            Console.WriteLine("*          *");
+            Console.WriteLine("************");
+
+            Console.WriteLine("Please enter your name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Please enter your age");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hi {0}! You are {1} years old", userName,age);
+
+            int i = 10;
+            Console.WriteLine("{0}*4={1}", i, i * 4);
+
+            Console.WriteLine("Please enter your salary");
+            double salary = Convert.ToDouble(Console.ReadLine());                                     // Convert string to double
+            double tax = 0.05 * salary;
+            Console.WriteLine("Your salary is {0:0,0.00}, your tax is {1:0,0.00}",salary,tax);        //format
+            Console.WriteLine("Your salary is {0:0,0.##}, your tax is {1:0,0.##}", salary, tax);      //##:optional
+            Console.WriteLine("Your salary is {0:#,###.00}, your tax is {1:#,###.00}", salary, tax);
+            Console.WriteLine("Your salary is {0:c}, your tax is {1:c}", salary, tax);
+
+
+
+        }
+    }
+}
---
+++ Day1Exercise/Day1Exercise/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Day1Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise1");
+            Console.WriteLine("Yi-Chieh Weng");
+            Console.WriteLine("yichieh.weng@gmail.com");
+
+            Console.WriteLine("**********************");
+            Console.WriteLine("Exercise2");
+            Console.WriteLine("Pleasr enter your name");
+            string userName=Console.ReadLine();
+            Console.WriteLine("Good Morning {0}",userName);
+
+            Console.WriteLine("**********************");
+            Console.WriteLine("Exercise3");
+            Console.WriteLine("Pleasr enter an interger");
+            int number =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine(number*number);
+
+            Console.WriteLine("**********************");
+            Console.WriteLine("Exercise4");
+            Console.WriteLine("Pleasr enter an number");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(number2 * number2);
+
+            Console.WriteLine("**********************");
+            Console.WriteLine("Exercise5");
+            Console.WriteLine("Pleasr enter an number");
+            double number3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0:0.00}",number3);
+
+
+
+
+        }
+    }
+}
