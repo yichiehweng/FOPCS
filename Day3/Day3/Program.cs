0004143cfc2d68d19e2dee4fb92d94b48b1461b2
Yi-Chieh Weng <yichieh.weng@gmail.com>
2017-08-07 18:37:04 +0800

    Day3

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Quiz");
+            Console.Write("Number to purchase:");
+            int qty = Convert.ToInt32(Console.ReadLine());
+            Excercise q1 = new Excercise();
+            double total = q1.Quiz(qty);
+            Console.WriteLine("The price would be ${0:0,0.00}",total);
+             
+            Excercise e1 = new Excercise();
+            e1.ForLoop();
+            e1.ForLoopForString();
+            
+            Console.WriteLine("While_Loop:");
+            Console.Write("Please enter how many you want to repeat:");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Excercise e2 = new Excercise();
+            e2.WhileLoop(j);
+            
+            Console.WriteLine("Tryparse:");
+            Excercise e3 = new Excercise();
+            e3.TryParse();
+            
+            Console.WriteLine("Tryparse2:");
+            Excercise e4 = new Excercise();
+            e4.TryParse2();
+            
+            Console.WriteLine("RandomNumber:");
+            Excercise e5 = new Excercise();
+            e5.Random();
+            
+            Excercise e6 = new Excercise();
+            e6.GuessGame();
+            
+            Excercise e7 = new Excercise();
+            e7.StringComparedTo();
+            e7.StringTrim();
+            
+            Excercise e8 = new Excercise();
+            e8.StringSubstring();
+            e8.StringInsert();
+            e8.StringPad();
+            e8.StringToLower();
+            e8.StringToUpper();
+            */
+            Excercise e9 = new Excercise();
+            e9.StringSplit();
+
+
+        }
+    }
+    class Excercise {
+        public double Quiz (int qty){
+            double gedgetPrice = 500;
+            
+            double discount = 0;
+            double total = qty * gedgetPrice;
+
+            if (total <= 3000) // Consider the dicision tree which is faster!
+            {
+                if (total <= 2000)
+                {
+                    discount = 0;
+                }
+                else
+                {
+                    discount = 0.03;
+                }
+            }else if (total <= 6000)
+            {
+                discount = 0.05;
+            }
+            else
+            {
+                discount = 0.08;
+            }
+            total = total * (1 - discount);
+            return total;
+
+
+
+}
+        public void ForLoop()
+        {
+            Console.WriteLine("For loop");
+            //first,declare a new variable i,initialize with 0;
+            //as long as I<10 we will repeat
+            //at the end of every repeatition we will do i++
+            //debug:F5 Start without debugging:Ctrl+F5 Next step: F10/F11 Stop:Ctrl+c
+            //normally for counting
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void ForLoopForString()
+        {
+            Console.WriteLine("For loop for String");
+            for (String s = ""; s.Length < 10; s = s + "*")
+            {
+                Console.WriteLine(s);
+            }
+        }
+        public void WhileLoop(int j)
+        {
+            int i = 0;
+            while (i < j)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+        public void TryParse() {
+            
+            bool isParsable = false;// user enter hasnt enter a interger
+            bool nothing = false;//user hasnt enter anything
+            int value=0;
+
+            while (nothing==false ||isParsable == false) {
+                
+                Console.Write("Please enter a interger you want to parse:");
+                string input = Console.ReadLine();
+                nothing = true;
+                isParsable = Int32.TryParse(input, out value);
+            }
+            Console.WriteLine("The value you enter is {0}", value);
+
+            /*
+           if (isParsable) {
+               Console.WriteLine("The value you enter is {0}",value);
+           }
+           else
+           {
+               Console.WriteLine("you didn't enter a interger");
+           }
+           */
+        }
+        public void TryParse2()
+        {
+            //first has different message
+            bool isParsable = false;// user enter hasnt enter a interger
+            bool nothing = false;//user hasnt enter anything
+            int value = 0;
+
+            while (nothing == false || isParsable == false)
+            {   
+                if (nothing==false)
+                {
+                    Console.Write("Please enter a interger you want to parse:");  
+                }
+                else
+                {
+                    Console.Write("Please enter again a interger you want to parse:");
+                }
+                string input = Console.ReadLine();
+                nothing = true;
+                isParsable = Int32.TryParse(input, out value);
+
+            }
+            Console.WriteLine("The value you enter is {0}", value);
+
+        }  
+        public void Random() {
+            Random r = new Random();
+            for (int i = 0;i< 10; i++){
+                int randomNumber = r.Next(0,100);
+                Console.WriteLine(randomNumber);
+            }
+        }
+        public void DoWhile()
+        {
+        }
+        public void GuessGame()
+        {
+            Random r = new System.Random();
+            int ans = r.Next(0, 10);
+            bool correctGuess = false;
+            do
+            {
+                Console.Write("Please guess(0-9):");
+                int guess = Int32.Parse(Console.ReadLine());
+                if (ans == guess)
+                {
+                    correctGuess = true;
+                }
+            } while (correctGuess == false);
+            {
+                Console.Write("Please guess again(0-9):");
+                int guess = Int32.Parse(Console.ReadLine());
+                if (ans == guess)
+                {
+                    correctGuess = true;
+                }
+                Console.WriteLine("Tour guess is true!");
+
+            }
+        }
+        public void StringComparedTo() {
+            Console.WriteLine("abc".CompareTo("xyz"));//-1
+            Console.WriteLine("ABC".CompareTo("xyz"));//-1
+            Console.WriteLine("abc".CompareTo("XYZ"));//-1
+            Console.WriteLine("abc".CompareTo("ABC"));//-1
+            Console.WriteLine("abc".CompareTo("abc"));//0
+            Console.WriteLine("ABC".CompareTo("abc"));//1
+            //Console.WriteLine("ABC".CompareTo(1));//Error!
+        }
+        public void StringTrim()
+        {
+            string s = " abc ";
+            Console.WriteLine("*" + s + "*");
+            string r = s.Trim();
+            Console.WriteLine("*" + r + "*");
+
+            string s2 = "$%$$abc%s%$";
+            char[] c = new char[] { '$', '%' }; // character want to trim: char array
+            Console.WriteLine("*" + s2 + "*");
+            string r2 = s.Trim(c);              // only trim from left and right, cannot trim middle character 
+            Console.WriteLine("*" + r2 + "*");
+            Console.WriteLine(c);
+            string r3 = s.TrimStart(c);
+            string r4 = s.TrimEnd(c);
+        }
+        public void StringSubstring()
+        {
+            string s = "Venkatraman";
+            string r = s.Substring(4, 3);
+            Console.WriteLine(r);
+            Console.WriteLine(s.Substring(6, 4));
+            string s1 = "Hello";
+            string last = s1.Substring(s1.Length - 1, 1);
+            Console.WriteLine(last);
+        }
+        public void StringInsert() {
+            string s = "Institute Systems Science";
+            string r = s.Insert(10, "of ");
+            Console.WriteLine(s);
+            Console.WriteLine(r);
+        }
+        public void StringPad()
+        {
+            string s = "ABC";
+            string r = s.PadLeft(7, 'c');// only single ''
+            Console.WriteLine(s);
+            Console.WriteLine(r);
+            Console.WriteLine(s.PadRight(6, 'z'));
+        }
+        public void StringToUpper() {
+            string s = "Venkat";
+            string r = s.ToUpper();
+            Console.WriteLine(s);
+            Console.WriteLine(r);
+        }
+        public void StringToLower() {
+            string s = "Venkat";
+            string r = s.ToLower();
+            Console.WriteLine(s);
+            Console.WriteLine(r);
+        }
+        public void StringSplit()
+        {
+            int i;
+            string a ="Institute of Systems Science";
+            string a2= "Institute  of Systems Science";
+            char[] chars = new char[] { ' ', 'i', '%' };
+            string[] s = a.Split(' ');
+            string[] s2 = a2.Split(chars);
+            for (i = 0; i <= s.Length - 1; i++)
+                Console.WriteLine(s[i]);
+            for (i = 0; i <= s2.Length - 1; i++)
+                Console.WriteLine(s2[i]);
+        }
+    }
+}
---
+++ Day3Exercise/Day3Exercise/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+
+namespace Day3Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Excercise1");
+            Console.Write("Please enter a number:");
+            string input = Console.ReadLine();
+            Excercise e1 = new Excercise();
+            e1.Excercise1(input);
+            
+            Console.WriteLine("Excercise2");
+            Console.Write("Please enter a numberA:");
+            int inputA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter a numberB:");
+            int inputB = Convert.ToInt32(Console.ReadLine());
+            Excercise e2 = new Excercise();
+            int LCM = e2.EuclidAlgorithm(inputA, inputB);
+            Console.WriteLine("LCM is {0}", LCM);
+            
+            Console.WriteLine("Excercise3");
+            Excercise e3 = new Excercise();
+            e3.GuessGame();
+            
+            Console.WriteLine("Excercise4");
+            Excercise e4 = new Excercise();
+            e4.SquareRootFinder();
+            
+            Console.WriteLine("Excercise1");
+            Excercise e5 = new Excercise();
+            e5.Factorial();
+            e5.Factorial2();
+            
+            Console.WriteLine("Excercise2");
+            Excercise e6 = new Excercise();
+            e6.Printer();
+            
+            Console.WriteLine("Excercise3");
+            Excercise e7 = new Excercise();
+            Console.Write("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = e7.CalcIsPrime(number);
+            Console.WriteLine(isPrime);
+            
+            Console.WriteLine("Excercise4");
+            Excercise e8 = new Excercise();
+            Console.Write("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isPerfect = e8.CalcIsPerfect(number);
+            Console.WriteLine(isPerfect);
+            
+            Excercise e9 = new Excercise();
+            e9.PrimePrinter();
+            
+            Excercise e10 = new Excercise();
+            e10.PerfectPrinter();
+            
+            Excercise e11 = new Excercise();
+            Console.WriteLine("Please enter a phase:");
+            string input = Console.ReadLine();
+            e11.VowelCounter(input);
+            
+            Excercise e12 = new Excercise();
+            Console.WriteLine("Please enter a phase:");
+            string input = Console.ReadLine();
+            bool ans = e12.Palindrome(input);
+            Console.WriteLine(ans);
+            */
+
+        }
+        class Excercise
+        {
+            public void Excercise1(string input)
+            {
+                while (input != "88")
+                {
+                    Console.Write("Please enter a number again:");
+                    input = Console.ReadLine();
+                }
+                Console.WriteLine("Lucky you...");
+            }
+            public int EuclidAlgorithm(int inputA, int inputB)
+            {
+                int A = inputA;
+                int B = inputB;
+                int LCM = 0;
+                int HCF = 0;
+                while (inputA != inputB)
+                {
+                    int changer = Math.Abs(inputA - inputB);
+                    if (inputA > inputB)
+                    {
+                        inputA = changer;
+                    }
+                    else
+                    {
+                        inputB = changer;
+                    }
+                }
+                HCF = inputA;
+                return LCM = (A * B) / HCF;
+            }
+            public void GuessGame()
+            {
+                Random rnd = new Random();
+                int ans = rnd.Next(0, 10);
+                int i = 0;
+                bool currectGuess= false;
+                do
+                {
+                    Console.Write("Please enter a number you guess(0-9):");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    if (ans == number)
+                    {
+                        currectGuess = true;
+                    }
+                    i++;
+
+                }
+                while (currectGuess == false);
+
+
+                if (i <= 2)
+                {
+                    Console.WriteLine("Correct!You are a Wizard!");
+                }
+                else if (i <= 5)
+                {
+                    Console.WriteLine("Correct!You are a good guess.");
+                }
+                else if (i <= 8)
+                {
+                    Console.WriteLine("Correct!You are lousy!");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                }
+
+
+            }
+            public void SquareRootFinder() {
+                Console.Write("Please enter number:");
+                int N = Convert.ToInt32(Console.ReadLine());
+                Random rnd = new Random();
+                bool isCorrect = false;
+                
+                    for (int i = 0; i < N; i++)
+                    {
+                        if (i * i == N)
+                        {
+                            Console.WriteLine(i);return;
+                        }
+                    }
+                
+                while (isCorrect == false)
+                {
+                    double G = rnd.NextDouble() * (N - 1) + 1;
+                    double formula = Math.Abs(G - (G + N / G) / 2);
+                    double number=Math.Round(formula*10000)/10000;
+                    if (number == 0)
+                    {
+                        isCorrect = true; 
+                    }
+                    if (isCorrect == true)
+                    {
+                        Console.WriteLine("{0:,0.000}", G);
+                    }
+                }
+                
+                
+
+            }
+            public void Factorial() {
+                Console.Write("Please enter an integer:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int ans = 1;
+                for(int i = 1; i <= number; i++)
+                {
+                    ans=ans*i;
+                }
+                Console.WriteLine(ans);
+            }
+            public void Factorial2()
+            {
+                Console.Write("Please enter an integer:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int ans = 1;
+                for (int i = number; i>0; i--)
+                {
+                    ans = ans * i;
+                }
+                Console.WriteLine(ans);
+            }
+            public void Printer() {
+                Console.Write("Please enter an integer:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                for (int i = 1;i<= number; i++) {
+                    double inverse=1/(double)i;
+                    double squareRoot = Math.Sqrt(i);
+                    double square = Math.Pow(i, 2);
+                    Console.WriteLine("number:{0},inverse number:{1},squareQoot:{2},square:{3}",i,inverse,squareRoot,square);
+                }
+            }
+            public bool CalcIsPrime(int number)
+            {
+
+                if (number == 1) return false;
+                if (number == 2) return true;
+
+                if (number % 2 == 0) return false; // Even number     
+
+                for (int i = 2; i < number; i++)
+                { // Advance from two to include correct calculation for '4'
+                    if (number % i == 0) return false;
+                }
+
+                return true;
+
+            }
+            public bool CalcIsPerfect(int number) {
+                bool isPerfect = false;
+                int k = 0;
+                for(int i = 1; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        k=k + i;
+                    }
+                }
+                if (k == number)
+                {
+                    isPerfect = true;
+                }
+                return isPerfect;
+            }
+            public void PrimePrinter()
+            {
+                
+                for (int i = 5; i<= 1000; i=i+2) {
+                    bool isPrime = true;
+                    for (int j = 3; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;break;
+                        }
+                    }
+                    if (isPrime == true)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            public void PerfectPrinter()
+            {
+                for (int i=1; i <= 1000; i++)
+                {
+                    bool isPerfect = false;
+                    int k = 0;
+                    for (int j = 1; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            k = k + j;
+                        }
+                    }
+                    if (k == i)
+                    {
+                        isPerfect = true;
+                    }
+                    if (isPerfect == true)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    
+                }
+            }
+            public void VowelCounter(string phase) {
+                char[] chars = phase.ToCharArray();
+                int counterA = 0,counterE=0,counterI=0,counterO=0,counterU=0,total=0;
+                for (int i = 0; i< chars.Length; i++)
+                {
+                    if(chars[i] =='A'|| chars[i] == 'a')
+                    {
+                        counterA++;
+                    }
+                }
+                Console.WriteLine(counterE);
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == 'E' || chars[i] == 'e')
+                    {
+                        counterE++;
+                    }
+                }
+                Console.WriteLine(counterE);
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == 'I' || chars[i] == 'i')
+                    {
+                        counterI++;
+                    }
+                }
+                Console.WriteLine(counterI);
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == 'O' || chars[i] == 'o')
+                    {
+                        counterO++;
+                    }
+                }
+                Console.WriteLine(counterO);
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (chars[i] == 'U' || chars[i] == 'u')
+                    {
+                        counterU++;
+                    }
+                }
+                Console.WriteLine(counterU);
+                total = counterA + counterE + counterI + counterO + counterU;
+                Console.WriteLine(total);
+
+
+            }
+            public bool Palindrome(string phase)
+            {
+                string first = phase.Substring(0, phase.Length / 2);
+                char[] arr = phase.ToCharArray();
+                Array.Reverse(arr);
+                string temp = new string(arr);
+                string second = temp.Substring(0, temp.Length / 2);
+                return first.Equals(second);
+
+            }
+
+
+        }
+    }
+}
